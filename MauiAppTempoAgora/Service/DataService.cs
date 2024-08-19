d3c2593c7896a281b4cd62f7a4502dc867006791
Usuario <Usuario@CPD03-M13>
2024-08-19 12:25:20 -0300

    Avanço no projeto

--- MauiAppTempoAgora/Service/DataService.cs
+++ MauiAppTempoAgora/Service/DataService.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MauiAppTempoAgora.Models;
-using System.Diagnostics;
-using Newtonsoft.Json;
+﻿using MauiAppTempoAgora.Models;
 using Newtonsoft.Json.Linq;
-using Java.Interop;
-using System.Data;
 
 namespace MauiAppTempoAgora.Service
 {
@@ -18,7 +9,7 @@ namespace MauiAppTempoAgora.Service
         {
             // https://openweathermap.org/current#current_JSON
             // https://home.openweathermap.org/api_keys
-            string appId = "";
+            string appId = "6135072afe7f6cec1537d5cb08a5a1a2";
 
             string url = $"http://api.openweathermap.org/data/2.5/weather?q={cidade}&units=metric&appid={appId}";
 
@@ -36,22 +27,28 @@ namespace MauiAppTempoAgora.Service
                     var rascunho = JObject.Parse(json);
 
                     DateTime time = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-                    DataTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
-                    DataTime sunset = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
 
                     // Ajustar de UTC para GMT
-                    // https://stckoverflow.com/questions/6239976/how-to-set-a-time-zone-
+                    // https://stackoverflow.com/questions/6239976/how-to-set-a-time-zone-or-a-kind-of-a-datetime-value
 
-                    tempo = new () 
-                    { 
+                    tempo = new()
+                    {
                         Humidity = (string)rascunho["main"]["humidity"],
                         Temperature = (string)rascunho["main"]["temp"],
                         Title = (string)rascunho["name"],
                         Visibility = (string)rascunho["visibility"],
-                    }
-                }
-            }
+                        Wind = (string)rascunho["wind"]["speed"],
+                        Sunrise = sunrise.ToString(),
+                        Sunset = sunset.ToString(),
+                        Weather = (string)rascunho["weather"][0]["main"],
+                        WeatherDescription = (string)rascunho["weather"][0]["description"],
+                    };
+                }// Fecha if
+            }// Fecha using
 
-        }
+            return tempo;
+        }// Fecha método
     }
 }
